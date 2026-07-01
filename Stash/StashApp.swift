946d60558518e733499b2f01979e7cae75b8026a
Sudeep Ogireddy <sudeepreddy@gmail.com>
2026-07-01 14:34:55 +0200

    Initial Commit

---
+++ Stash/StashApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashApp.swift
+//  Stash
+//
+//  Created by Sree Sudeep Reddy Ogireddy on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct StashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
